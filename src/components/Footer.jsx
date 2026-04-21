951812168aafddaddb37cc995cebbf2f7db0137c
Kilian <dankestmirror@hotmail.es>
2026-04-21 15:24:36 +0100

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="text-slate-400 py-6 text-center text-sm">
-      <p>© 2025 Orbix - Space Explorer Dashboard</p>
+    <footer className=" text-slate-400 py-6 text-center text-sm">
+      <p>© 2026 Orbix - Space Explorer Dashboard</p>
     </footer>
   );
 }
