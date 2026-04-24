39dcbec463e55d883d9b24237281e4142dc7fba1
Kilian <dankestmirror@hotmail.es>
2026-04-24 10:40:39 +0100

    Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 import "./App.css";
 
 // Componentes Globales
@@ -11,43 +12,45 @@ import LandigStats from "./components/LandigStats";
 import ComponetAstronautas from "./components/ComponetAstronautas";
 import { OrbixProvider } from "./context/OrbixContext";
 import AstroPage from "./pages/AstroPage";
-
+import LaunchesPage from "./pages/LaunchesPage";
+import CommanderDashboard from "./pages/CommanderDashboard";
 // Proveedor de Contexto (Tu requisito de "API en Context")
 /* import { OrbixProvider } from "./context/OrbixContext"; */
 
 function App() {
+  const [commander, setCommander] = useLocalStorage("orbix_commander", null);
+
   return (
     <OrbixProvider>
       <Router>
         <div className="flex flex-col min-h-screen bg-slate-950">
-          <Header />
-
+          <Header commander={commander} />
           <main className="flex-grow">
-            {" "}
-            {/* pt-20 para que el Header fixed no tape el contenido */}
             <Routes>
-              {/* RUTA PRINCIPAL (Home) */}
+              {/* RUTA PRINCIPAL: Aquí pasamos la función a los dos sitios con login */}
               <Route
                 path="/"
                 element={
                   <>
-                    <HeroSection />
+                    <HeroSection onLoginSuccess={setCommander} /> 
                     <div className="max-w-screen-2xl mx-auto">
-                      <LandigStats />
+                      <LandigStats onLoginSuccess={setCommander} />
                       <ComponetAstronautas />
                     </div>
                   </>
                 }
               />
+
               <Route path="/astronauts" element={<AstroPage />} />
+              <Route path="/launches" element={<LaunchesPage />} />
 
-              {/* RUTA DE DETALLE (Requisito: Params ID) */}
-              {/* <Route path="/astronaut/:id" element={<AstronautDetail />} /> */}
-
-              {/* Puedes añadir más rutas aquí, como /launches */}
+              {/* CORRECCIÓN: Esta ruta debe cargar el Dashboard, no las Stats */}
+              <Route 
+                path="/commander-dashboard" 
+                element={<CommanderDashboard commander={commander} setCommander={setCommander} />} 
+              />
             </Routes>
           </main>
-
           <Footer />
         </div>
       </Router>
